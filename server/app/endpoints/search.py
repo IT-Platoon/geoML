26c3d034e83eb0f6072913f8da5117c42042df62
Семён Замолоцкий <Semrole@yandex.ru>
2023-08-27 01:50:46 +0700

    change colors

--- server/app/endpoints/search.py
+++ server/app/endpoints/search.py
@@ -1,5 +1,11 @@
 from fastapi import APIRouter, Query, Request, responses, status
 
+def example(count: int = 10):
+    return [
+        {"id": i+1, "address": f'г. Санкт-Петербург, ул. Достоевского, д. {i+1} литера Е'}
+        for i in range(count)
+    ]
+
 api_router = APIRouter(
     prefix="/search",
     tags=["geo_ml"],
@@ -16,8 +22,5 @@ async def search(
 ):
     return {
         "query": address,
-        "result" : [
-            {"id":1, "address": 'г. Санкт-Петербург, ул. Достоевского, д. 44 литера Е'},
-        ] * count
-        
+        "result" : example(count)        
     }
